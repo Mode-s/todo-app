23f58439460d833841ec7bfb31a6ae657bffd590
shotaumemoto <carachandes07@gmail.com>
2026-07-02 01:02:14 +0900

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const customFont = localFont({
+  src: "../fonts/nicomoji-plus_v2-5.ttf",
+  display: "swap",
+  variable: "--font-custom",
 });
 
 export const metadata: Metadata = {
@@ -23,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ja" className={customFont.variable}>
       <body>{children}</body>
     </html>
   );
